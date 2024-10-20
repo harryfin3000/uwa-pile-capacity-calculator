d4b6f72fc44ae27bbe823d408037474609c79d5c
Robin Lehane <robinlehane@hotmail.com>
2024-10-20 22:41:32 +1100

    Updates

---
+++ precache-manifest.757ae425687eb3ab60535058863f3fed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7065c68d6fc6e4439320ae3c0e8e116",
+    "url": "/uwa-pile-capacity-calculator/index.html"
+  },
+  {
+    "revision": "c249e32e89dc0327b6b0",
+    "url": "/uwa-pile-capacity-calculator/static/css/main.ab5edfab.chunk.css"
+  },
+  {
+    "revision": "92e23421ff612ea251ce",
+    "url": "/uwa-pile-capacity-calculator/static/js/2.8851f76e.chunk.js"
+  },
+  {
+    "revision": "c249e32e89dc0327b6b0",
+    "url": "/uwa-pile-capacity-calculator/static/js/main.c75670a1.chunk.js"
+  },
+  {
+    "revision": "87031fdf7b76ff40af5c",
+    "url": "/uwa-pile-capacity-calculator/static/js/runtime~main.d5c49884.js"
+  }
+]);
